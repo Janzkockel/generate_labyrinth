66d041c2fec41e933a06fd85eea775e02e391bdf
root <root@DESKTOP-7CSI0AE.localdomain>
2025-09-15 21:52:34 +0200

    started path finding, NOT FINISHED

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <cstdlib>
-#include <ctime> 
+#include <ctime>
 
 void draw_rect(int32_t x, int32_t y, int32_t width, int32_t height, double r,
                double g, double b);
@@ -13,14 +13,49 @@ int32_t window_width = 1000;
 const int32_t cols = 30;
 const int32_t rows = 30;
 
-bool pathExists(){
-    static uint16_t timer = 0;
+bool pathExists(uint8_t start_col, uint8_t start_row)
+{
+    enum direction_enum
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    };
+    enum direction_enum direction = RIGHT;
 
-    if(timer <= 300){
-        timer++;
-        return 0;
+    uint8_t current_pos[2];
+    current_pos[0] = start_col;
+    current_pos[1] = start_row;
+
+    if (current_pos == ){
+
     }
-    return 1;
+
+    switch (direction)
+    {
+    case RIGHT:
+
+        break;
+    
+    case LEFT:
+    
+        break;
+
+    case DOWN:
+        
+        break;
+
+    case UP:
+        
+        break;
+    
+    default:
+        printf("direction undef");
+        break;
+    }
+
+    return 0;
 }
 
 int main()
@@ -29,20 +64,36 @@ int main()
     srand(time(NULL));
     uint8_t gameboard[cols][rows];
 
-    for (uint8_t cols_g = 0; cols_g <= cols-1; cols_g++){ // _g = generate
-        for (uint8_t rows_g = 0; rows_g <= rows-1; rows_g++){
+    const uint8_t start[2] = {0, 6}; // col, row
+    const uint8_t target[2] = {29, 25};
+    for (uint8_t cols_g = 0; cols_g <= cols - 1; cols_g++)
+    { // _g = generate
+        for (uint8_t rows_g = 0; rows_g <= rows - 1; rows_g++)
+        {
             gameboard[cols_g][rows_g] = 1;
         }
     }
 
-    while (!pathExists())
+    gameboard[start[0]][start[1]] = 8;
+    gameboard[target[0]][target[1]] = 9;
+
+    while (!pathExists(start[0], start[1]))
     {
         uint8_t r_col = rand() % cols;
         uint8_t r_row = rand() % rows;
 
-        gameboard[r_col][r_row] = 0;
-    }   
-    
+        if (r_col == start[0] && start[1] == 6)
+        {
+            gameboard[start[0]][start[1]] = 8;
+        }
+        else if (r_col == target[0] && r_row == target[1])
+        {
+            gameboard[target[0]][target[1]] = 8;
+        }
+        else{
+            gameboard[r_col][r_row] = 0;
+        }
+    }
 
     // GLFWwindow *window = glfwCreateWindow(window_width, window_height, "generate Labyrinth", NULL, NULL);
 
@@ -61,8 +112,10 @@ int main()
 
     // glfwTerminate();
 
-    for (uint8_t cols_g = cols-1; cols_g > 0; cols_g--){ // _g = generate
-        for (uint8_t rows_g = rows-1; rows_g > 0; rows_g--){ // _g = generate
+    for (uint8_t cols_g = cols - 1; cols_g > 0; cols_g--)
+    { // _g = generate
+        for (uint8_t rows_g = rows - 1; rows_g > 0; rows_g--)
+        { // _g = generate
             printf("%i ", gameboard[cols_g][rows_g]);
         }
         printf("\n");
